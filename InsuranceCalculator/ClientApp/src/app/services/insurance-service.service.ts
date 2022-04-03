02b30a3b0073404a7f8bc50367d6ba2f66dae0c0
Sarvabowmen <sarvabowmen@gmail.com>
2022-04-03 23:56:53 +0530

    Integrated HttpClient and called the api endpoint to get the death premium and displayed in the UI

--- InsuranceCalculator/ClientApp/src/app/services/insurance-service.service.ts
+++ InsuranceCalculator/ClientApp/src/app/services/insurance-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { InsuranceRequest } from '../models/insuranceRequest';
 @Injectable({
   providedIn: 'root'
 })
-export class InsuranceServiceService {
+export class InsuranceService {
+    public deathPremiumAmount: number = 0;
+  constructor(private httpClient: HttpClient) {
 
-  constructor() { }
+   }
+   public calculateSumAssured(data:InsuranceRequest): Observable<number>{
+    return this.httpClient.post<number>('/api/insurance', data);
+  }
 }
