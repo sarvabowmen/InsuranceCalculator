b8f46bae1469272554f4a6c86bdea40ba6e8a6fc
Sarvabowmen <sarvabowmen@gmail.com>
2022-04-03 22:43:16 +0530

    Added basic validations and error messages

--- InsuranceCalculator/ClientApp/src/app/insurance-calculator-form/insurance-calculator-form.component.ts
+++ InsuranceCalculator/ClientApp/src/app/insurance-calculator-form/insurance-calculator-form.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-insurance-calculator',
   templateUrl: './insurance-calculator-form.component.html',
 })
-export class InsuranceCalucluatorForm {
-    options: FormGroup;
+export class InsuranceCalucluatorForm implements OnInit{
+    insuranceForm: FormGroup;
+    submitted = false;
+
     floatLabelControl = new FormControl('auto');
 
-    constructor(fb: FormBuilder) {
-        this.options = fb.group({
-          name: ['', Validators.required],
-          age: ['', Validators.required],
-          dateOfBirth: ['', Validators.required],
-          occupation: ['', Validators.required],
-          sumAssured: ['', Validators.required]
-        },{
-          floatLabel: this.floatLabelControl,
-        });
+    constructor(private fb: FormBuilder) {
+        
+    }
+    ngOnInit(){
+      this.insuranceForm = this.fb.group({
+        name: ['', Validators.required],
+        age: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+        occupation: ['', Validators.required],
+        sumAssured: ['', Validators.required]
+      },{
+        floatLabel: this.floatLabelControl,
+      });
+
+    }
+
+    get insuranceFormControl() {
+      return this.insuranceForm.controls;
+    }
+    
+    onSubmit(){
+      if (this.insuranceForm.valid) {
+        alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+        console.table(this.insuranceForm.value);
+      }
     }
 
 }
